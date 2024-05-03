1bfc3950ef6b138a7a40452e51ff7c1d0c8f5066
celsoeduardobentopugim <140861101+celsoeduardobentopugim@users.noreply.github.com>
2024-05-02 21:30:55 -0300

    Add files via upload

---
+++ primeira aula java/script.js
@@ -0,0 +1,103 @@
+
+
+var nome= "celso eduardo";
+
+let sobrenome = "pugim";
+
+console.log(nome);
+
+console.log(sobrenome);
+
+if(true){
+    var nome2="maria 3"
+}
+//console.log(nome3);
+
+if(true){
+    let nome2="maria 2"
+}
+console.log(nome2);
+
+//let nome= "matheus";//string
+
+let numero= 1;//inteiro
+
+let numero2= 1.15;//decimais
+
+let verdade= true // booleano
+
+let texto;//indefinido;
+
+let texto02 = null; //vazio
+
+
+//adição
+
+let num1 = 10;
+let num2 = 5;
+
+//adição
+console.log(num1 + num2)
+//subtração
+console.log(num1 - num2)
+//multiplicação
+console.log(num1 * num2)
+//divição
+console.log(num1 /num2)
+// potencia
+console.log(num1 **num2)
+//modulo
+console.log(num1 % num2);
+
+// operadores de comparação
+
+
+console.log(5==5);//igual
+
+console.log(5===5);//identico
+
+console.log(5>7);//falso maior
+
+console.log(5<7);//verdadeiro menor
+
+console.log(5<=5);//verdadeiro maior e igual
+
+console.log(5>=5);//falso maior e igual
+
+console.log(5!=5);//falso diferente
+
+console.log(5!==5);//falso  não identico
+
+//operadores logicos
+
+
+/*
+
+e &&
+
+ou||
+
+não !
+
+*/
+
+let a = 5;
+let b =10;
+
+console.log(a>0 && b >5);//verdadeiro
+
+console.log(a>0 || b >15);//verdadeiro
+
+
+console.log(a>3  && !b >-50);//verdadeiro
+
+console.log(!b);//falso
+
+let resposta = confirm("o palmeiras não tem mundial ?");
+console.log(resposta);
+
+let resposta2 = prompt ("qual o seu nome ");
+console.log(resposta2);
+
+
+
